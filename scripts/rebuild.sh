70ad44bdebe6f6ccf89b717b3ad253623b3ecbfd
anuj04patwal <anuj.patwal@sutara.org>
2026-01-30 10:21:46 +0530

    rebuild

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,8 +1,14 @@
-rm -rf public/*
-mkdir -p public
+#!/bin/bash
+set -euxo pipefail
+
+PUBLIC_DIR="public"
+rm -rf "$PUBLIC_DIR"/*
+mkdir -p "$PUBLIC_DIR"
 
 for zip in *.zip; do
   NAME="$(basename "$zip" .zip)"
-  mkdir -p "public/$NAME"
-  unzip -o "$zip" -d "public/$NAME"
+  mkdir -p "$PUBLIC_DIR/$NAME"
+  unzip -o "$zip" -d "$PUBLIC_DIR/$NAME"
 done
+
+ls -R public | head -300
